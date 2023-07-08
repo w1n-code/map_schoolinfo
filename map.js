f0c87c349054968075cde0d370a37a3a2967e9f1
w1n-code <131504642+w1n-code@users.noreply.github.com>
2023-07-09 02:10:37 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,52 @@
-var mapContainer = document.getElementById('map');
-var mapOptions = {
-  center: new kakao.maps.LatLng(37.5665, 126.9780), // Set the initial map center
-  level: 5 // Set the initial zoom level
-};
-var map = new kakao.maps.Map(mapContainer, mapOptions);
+  var mapContainer = document.getElementById('map');
+  var mapOptions = {
+    center: new kakao.maps.LatLng(35.887238, 128.585070),
+    level: 6
+  };
 
-var markerPosition = new kakao.maps.LatLng(37.5665, 126.9780); // Set the marker position
-var marker = new kakao.maps.Marker({
-  position: markerPosition
-});
-marker.setMap(map); // Add the marker to the map
+  var map = new kakao.maps.Map(mapContainer, mapOptions);
+
+  var data = [
+    [35.887238, 128.585070, 'Daegu Iljung'],
+    [35.880818, 128.598612, 'Kyungmyeong Girls Middle School'],
+    [35.881453, 128.576243, 'Gyeongil Middle School']
+  ];
+
+  var markers = [];
+  var overlays = [];
+
+  for (var i = 0; i < data.length; i++) {
+    var marker = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(data[i][0], data[i][1])
+    });
+
+    var overlay = new kakao.maps.CustomOverlay({
+      content:
+        '<div class="wrap">' +
+        ' <div class="info">' +
+        '   <div class="title">' +
+        data[i][2] +
+        '     <div class="close" onclick="closeOverlay(' + i + ')" title="close"></div>' +
+        '   </div>' +
+        ' </div>' +
+        '</div>',
+      map: map,
+      position: new kakao.maps.LatLng(data[i][0], data[i][1])
+    });
+
+    markers.push(marker);
+    overlays.push(overlay);
+  }
+
+  function closeOverlay(index) {
+    overlays[index].setMap(null);
+  }
+
+  for (var i = 0; i < markers.length; i++) {
+    kakao.maps.event.addListener(markers[i], 'click', (function (index) {
+      return function () {
+        overlays[index].setMap(map);
+      };
+    })(i));
+  }
