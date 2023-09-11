63cda379863a9140eafeb7bea3d2fa3447789737
Taewon Yang <for.code.won@gmail.com>
2023-09-12 00:11:06 +0900

    Data 내용 구조가 바뀌어도 실행되도록

--- map.js
+++ map.js
@@ -7,15 +7,14 @@ var mapOptions = {
 var map = new kakao.maps.Map(mapContainer, mapOptions);
 
 var data = [
-  [35.887238, 128.585070, '대구일중학교', 'https://tgil.dge.ms.kr', '8/21', '9/26~9/27 | 2,3학년', '11/1~11/3 | 3학년', '11/29~12/1 | 2학년', '24.2/6 (겨울방학: 12/30~24.2/1)'],
-  [35.880818, 128.598612, '경명여자중학교', 'https://kmschool.dge.ms.kr', '7/27', '9/21~9/22 | 2,3학년', '10/31~11/2 | 3학년', '11/27~11/29 | 2학년', '12/15'],
-  [35.881453, 128.576243, '경일중학교','https://e-kyungil.dge.ms.kr', '8/16', '9/25~9/26 | 2,3학년', '11/1~11/3 | 3학년', '12/6~12/8 | 2학년', '2/1 (겨울방학: 12/30~24.1/28)'],
-  [35.889879, 128.588891, '침산중학교', 'https://chimsan.dge.ms.kr', '8/18', '9/26~9/27 | 2,3학년', '11/1~11/3 | 3학년', '11/29~12/1 | 2학년', '2/7 (겨울방학: 12/30~24.1/31)'],
-  [35.858528, 128.584136, '대구경구중학교', 'https://gyeonggu.dge.ms.kr', '8/16', '9/26~9/27 | 2,3학년', '11/1~11/3 | 3학년', '12/6~12/8 | 2학년', '24.2/7 (겨울방학: 12/28~24.1/31)'],
-[35.852400, 128.586360, '경상중학교', 'https://kyungsang.dge.ms.kr', '8/17', '9/21~9/22 | 2,3학년', '10/31~11/2 | 3학년', '12/5~12/7 | 2학년', '24.2/7(겨울방학: 12/29~24.1/31)'],
-[35.867161, 128.581208, '계성중학교', 'https://keisung.dge.ms.kr', '8/16', '9/26~6/27 | 2,3학년', '11/2~11/6 | 3학년', '12/6~12/8 | 2학년', '24.2/6(겨울방학: 12/29~24.1/31)']
+  [35.887238, 128.585070, '대구일중학교', 'https://tgil.dge.ms.kr', '개학식: 8/21', '중간고사: 9/26~9/27 | 2,3학년', '기말고사: 11/1~11/3 | 3학년', '기말고사: 11/29~12/1 | 2학년', '종업/졸업식: 24.2/6 (겨울방학: 12/30~24.2/1)'],
+  [35.880818, 128.598612, '경명여자중학교', 'https://kmschool.dge.ms.kr', '개학식: 7/27', '중간고사: 9/21~9/22 | 2,3학년', '기말고사: 10/31~11/2 | 3학년', '기말고사: 11/27~11/29 | 2학년', '종업/졸업식: 12/15'],
+  [35.881453, 128.576243, '경일중학교','https://e-kyungil.dge.ms.kr', '개학식: 8/16', '중간고사: 9/25~9/26 | 2,3학년', '기말고사: 11/1~11/3 | 3학년', '기말고사: 12/6~12/8 | 2학년', '종업/졸업식: 2/1 (겨울방학: 12/30~24.1/28)'],
+  [35.889879, 128.588891, '침산중학교', 'https://chimsan.dge.ms.kr', '개학식: 8/18', '중간고사: 9/26~9/27 | 2,3학년', '기말고사: 11/1~11/3 | 3학년', '기말고사: 11/29~12/1 | 2학년', '종업/졸업식: 2/7 (겨울방학: 12/30~24.1/31)'],
+  [35.858528, 128.584136, '대구경구중학교', 'https://gyeonggu.dge.ms.kr', '개학식: 8/16', '중간고사: 9/26~9/27 | 2,3학년', '기말고사: 11/1~11/3 | 3학년', '기말고사: 12/6~12/8 | 2학년', '종업/졸업식: 24.2/7 (겨울방학: 12/28~24.1/31)'],
+[35.852400, 128.586360, '경상중학교', 'https://kyungsang.dge.ms.kr', '개학식: 8/17', '중간고사: 9/21~9/22 | 2,3학년', '기말고사: 10/31~11/2 | 3학년', '기말고사: 12/5~12/7 | 2학년', '종업/졸업식: 24.2/7(겨울방학: 12/29~24.1/31)'],
+[35.867161, 128.581208, '계성중학교', 'https://keisung.dge.ms.kr', '개학식: 8/16', '중간고사: 9/26~6/27 | 2,3학년', '기말고사: 11/2~11/6 | 3학년', '기말고사: 12/6~12/8 | 2학년', '종업/졸업식: 24.2/6(겨울방학: 12/29~24.1/31)']
 ];
-
 var markers = [];
 var overlays = [];
 
@@ -66,25 +65,11 @@ for (var i = 0; i < data.length; i++) {
   liTest1.appendChild(aTag);
   ul.appendChild(liTest1);
 
-  var liOpening = document.createElement('li');
-  liOpening.textContent = '개학식: ' + data[i][4];
-  ul.appendChild(liOpening);
-
-  var liMidterm = document.createElement('li');
-  liMidterm.textContent = '중간고사: ' + data[i][5];
-  ul.appendChild(liMidterm);
-    
-  var liFinterm3 = document.createElement('li');
-  liFinterm3.textContent = '기말고사: ' + data[i][6];
-  ul.appendChild(liFinterm3);
-    
-  var liFinterm2 = document.createElement('li');
-  liFinterm2.textContent = '기말고사: ' + data[i][7];
-  ul.appendChild(liFinterm2);
-
-  var Closing = document.createElement('li');
-  Closing.textContent = '종업/졸업식: ' + data[i][8];
-  ul.appendChild(Closing);
+  for(let j=0; j < data[i].length; j++){
+      var some = document.createElement('li');
+  		some.textContent = data[i][j];
+ 			ul.appendChild(some);
+  }
 
   explainDiv.appendChild(ul);
   infoDiv.appendChild(explainDiv);
